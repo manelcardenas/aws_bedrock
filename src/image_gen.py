4ab1b2fa6bebce81681d4fc0bbc2b895b3b184f4
manelcardenas <manelcardenas5@gmail.com>
2025-08-30 12:45:45 +0200

    feat: stability image model creation

---
+++ src/image_gen.py
@@ -0,0 +1,39 @@
+import boto3
+import os
+import json
+from dotenv import load_dotenv
+import base64
+
+load_dotenv()
+
+client = boto3.client(
+    service_name="bedrock-runtime", region_name=os.getenv("AWS_REGION")
+)
+
+stability_image_config = json.dumps(
+    {
+        "prompt": "A photo of a cat",
+    }
+)
+
+# Invoke the model with the request.
+response = client.invoke_model(
+    modelId=os.getenv("IMAGE_MODEL_ID"),
+    body=stability_image_config,
+    accept="application/json",
+    contentType="application/json",
+)
+
+
+response_body = json.loads(response.get("body").read())
+base64_image_data = response_body.get("images")[0]
+
+# Extract the image data.
+image_data = base64.b64decode(base64_image_data)
+
+# Save the generated image to a local folder.
+file_name = "/images/stability_image_2.png"
+with open(file_name, "wb") as file:
+    file.write(image_data)
+
+print(f"The generated image has been saved to {file_name}")
--- src/main.py
+++ src/main.py
@@ -6,36 +6,49 @@ from botocore.exceptions import ClientError
 
 load_dotenv()
 
+history = []
+
+def get_history():
+        return "\n".join(history)
+
+def get_config():
+     return json.dumps({
+        "inputText": get_history(),
+        "textGenerationConfig": {
+            "maxTokenCount": 512,
+            "temperature": 0,
+        }
+     })
 
 def main():
     client = boto3.client(service_name="bedrock-runtime", region_name=os.getenv("AWS_REGION"))
 
     model_id = os.getenv("MODEL_ID")
 
-    prompt = "Explain the concept of quantum computing in a way that is easy to understand."
+    while True:
+        try:
+            user_input = input("You: ")
+            if user_input.lower() in ["exit", "quit", "bye"]:
+                break
+            if not user_input.strip():
+                continue
+            history.append("User: " + user_input)
+            response = client.invoke_model(
+                modelId=model_id,
+                body=get_config(),
+                accept="application/json",
+                contentType="application/json",
+            )
+            model_response = json.loads(response["body"].read())
+            results = model_response.get("results", [])
+            output_text = results[0].get("outputText", "").strip()
+            print(output_text)
+            history.append(output_text)
+                    
+        except (ClientError, Exception) as e:
+            print(f"ERROR: Can't invoke '{model_id}'. Reason: {e}")
+            exit(1)
 
-
-    request = json.dumps({
-        "prompt": prompt,
-        "max_gen_len": 512,
-        "temperature": 0,
-    })
-
-    try:
-        # Invoke the model with the request.
-        streaming_response = client.invoke_model_with_response_stream(
-            modelId=model_id, body=request
-        )
-
-        # Extract and print the response text in real-time.
-        for event in streaming_response["body"]:
-            chunk = json.loads(event["chunk"]["bytes"])
-            if "generation" in chunk:
-                print(chunk["generation"], end="")
-
-    except (ClientError, Exception) as e:
-        print(f"ERROR: Can't invoke '{model_id}'. Reason: {e}")
-        exit(1)
 
 
 if __name__ == "__main__":
